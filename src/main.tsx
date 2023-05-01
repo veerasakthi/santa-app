1a90941ec98aea6ebb4d847fd8c14d75ce1ba550
Veera Sakthi <veeranex@gmail.com>
2023-05-01 16:32:21 +0900

    initial commit
    added tailwind css and removed unwanted code from reactjs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import SendLetter from "./components/SendLetter";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>{/* Missing code */}</React.StrictMode>
+  <React.StrictMode>
+    <SendLetter />
+  </React.StrictMode>
 );
